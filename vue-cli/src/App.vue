eebf0abf49816e778664da676c6eccc7f7bb4b60
xiaomian <812083426@qq.com>
2019-02-21 23:30:39 +0800

    submit code

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,46 +1,11 @@
 <template>
-    <div id="app">
-        <img src="./assets/logo.png">
-        <div>
-            <p>
-                If Element is successfully added to this project, you'll see an
-                <code
-                    v-text="'<el-button>'"
-                ></code>
-                below
-            </p>
-            <el-button v-if="inlist in list">el-button</el-button>
-        </div>
-    </div>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-
-export default {
-    name: 'app',
-    data(){
-        return{
-            inlist:'122',
-            list:['1','2','3']
-        }
-    },
-    components: {
-    },
-    created () {
-        this.$http.get('/test').then(res => {
-            console.log(res);
-        })
-    }
+export default{
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
--- vue-cli/src/store/index.js
+++ vue-cli/src/store/index.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import app from './modules/app'
+import errorLog from './modules/errorLog'
+import permission from './modules/permission'
+import tagsView from './modules/tagsView'
+import user from './modules/user'
+import getters from './getters'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-
+const store = new Vuex.Store({
+  modules: {
+    app,
+    errorLog,
+    permission,
+    tagsView,
+    user
   },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  getters
 })
+
+export default store
