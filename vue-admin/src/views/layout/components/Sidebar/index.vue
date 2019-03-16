fd7454c4fff085b552da5592d4f8d30cad71a5a3
贺敏 <812083426@qq.com>
2019-03-16 18:08:27 +0800

    commit

--- vue-admin/src/views/layout/components/Sidebar/index.vue
+++ vue-admin/src/views/layout/components/Sidebar/index.vue
@@ -27,47 +27,9 @@
                     <el-menu-item index="1-4-1">选项1</el-menu-item>
                 </el-submenu>
             </el-submenu>
-            <el-submenu index="1">
-                <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span slot="title">导航一</span>
-                </template>
-                <el-menu-item-group>
-                    <span slot="title">分组一</span>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                    <span slot="title">选项4</span>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
-                </el-submenu>
-            </el-submenu>
             <el-menu-item index="2">
                 <i class="el-icon-menu"></i>
                 <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-                <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
             </el-menu-item>
         </el-menu>
     </div>
@@ -89,8 +51,8 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-.sidebar { 
+<style lang="less">
+.sidebar {
     height: 100%;
     position: fixed;
     font-size: 0px;
@@ -100,6 +62,9 @@ export default {
     z-index: 1001;
     overflow: hidden;
 
+    .el-submenu__title {
+        width: 180px;
+    }
     .menu {
         overflow: auto;
         height: 100%;
