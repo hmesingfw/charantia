3a2be1ccba21fd43a7715ee68d406f488ca03052
xiaomian <812083426@qq.com>
2019-09-16 15:38:13 +0800

    0.5 version

--- nodejs/koa-ejs-generate-vue-element/routes/users.js
+++ nodejs/koa-ejs-generate-vue-element/routes/users.js
@@ -1,15 +1,16 @@
+const uuidv1 = require('uuid/v1');
 const router = require('koa-router')();
-
 const db = require('../utils/sequelize');
 const Sequelize = db.sequelize();
 const sysMain = Sequelize.import('../model/sys_main');
 const Op = require('sequelize').Op;
 router.prefix('/users')
 
+
 router.get('/', async (ctx, next) => {
     await ctx.render('index')
 })
-
+/* 查询列表 */
 router.post('/list', async (ctx, next) => {
     let form = {
         ...ctx.request.body
@@ -39,11 +40,36 @@ router.post('/list', async (ctx, next) => {
             }
         },
     }
-
 })
+/* 新增修改 */
+router.post('/edit', async (ctx, next) => {
+    let form = {
+        ...ctx.request.body
+    }
+    let suc = '';
+    if (form.id) {
+        /* 修改 */
+        suc = await sysMain.update(form, { where: { id: form.id } })
+    } else {
+        /* 新增 */
+        form.id = uuidv1();
+        suc = await sysMain.create(form);
+    }
 
-router.get('/bar', function (ctx, next) {
-    ctx.body = 'this is a users/bar response'
+    ctx.body = {
+        code: 200,
+        data: suc
+    }
+})
+/* 删除 */
+router.post('/delete', async (ctx) => {
+    let idArr = ctx.request.body.id;
+    let suc = await sysMain.destroy({ where: { 'id': idArr } })
+    console.log(suc);
+    ctx.body = {
+        code: 200,
+        data: suc
+    }
 })
 
 module.exports = router
