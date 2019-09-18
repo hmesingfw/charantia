6f38b725ac78e9c2374b0c0aba5993ee737a1aba
xiaomian <812083426@qq.com>
2019-09-18 17:36:43 +0800

    .053

--- nodejs/koa-ejs-generate-vue-element/sql/generate_table.sql
+++ nodejs/koa-ejs-generate-vue-element/sql/generate_table.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50712
 File Encoding         : 65001
 
-Date: 2019-09-16 15:56:09
+Date: 2019-09-18 17:36:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,6 +28,7 @@ CREATE TABLE `sys_body` (
 -- ----------------------------
 -- Records of sys_body
 -- ----------------------------
+INSERT INTO `sys_body` VALUES ('48ceaf30-d9df-11e9-99e7-13b3ae4ad8f7', null);
 
 -- ----------------------------
 -- Table structure for sys_main
@@ -35,19 +36,19 @@ CREATE TABLE `sys_body` (
 DROP TABLE IF EXISTS `sys_main`;
 CREATE TABLE `sys_main` (
   `id` varchar(36) NOT NULL,
-  `body_id` varchar(32) DEFAULT NULL,
+  `body_id` varchar(36) DEFAULT NULL,
   `title` varchar(64) DEFAULT NULL COMMENT '名称',
   `path` varchar(255) DEFAULT NULL COMMENT '路径',
   `is_cdn` char(1) DEFAULT NULL COMMENT '0 是   1   否',
   `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `main_body` (`body_id`),
+  CONSTRAINT `main_body` FOREIGN KEY (`body_id`) REFERENCES `sys_body` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_main
 -- ----------------------------
-INSERT INTO `sys_main` VALUES ('1', '1', '测试1', '1', '1', '1', '2019-09-11 10:40:00', '2019-09-11 10:40:04');
-INSERT INTO `sys_main` VALUES ('4', '1', '测试4', '1', '1', '1', '2019-09-11 10:40:00', '2019-09-12 10:40:04');
-INSERT INTO `sys_main` VALUES ('9919b890-d84a-11e9-97ba-6de424d37f26', null, 'cse', null, '1', null, '2019-09-16 06:24:11', '2019-09-16 06:24:11');
+INSERT INTO `sys_main` VALUES ('48ceaf30-d9df-11e9-99e7-13b3ae4ad8f7', '48ceaf30-d9df-11e9-99e7-13b3ae4ad8f7', '测试功能', null, '1', null, '2019-09-18 06:41:03', '2019-09-18 06:41:03');
