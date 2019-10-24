f76d8c56574c16a1137322757da988e827987322
xiaomian <812083426@qq.com>
2019-10-24 11:14:19 +0800

    0.61

--- DOClevel/Server/patch/4.1.0.js
+++
@@ -1,9 +0,0 @@
-
-
-var admin=require("../model/adminModel")
-module.exports=async function () {
-    await (admin.createAsync({
-        name:"DOClever",
-        password:"DOClever"
-    }));
-}
--- DOClevel/Server/third/requestAsync.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by sunxin on 16/6/8.
- */
-var request=require("request");
-var blue=require("bluebird");
-var requestAsync=blue.promisify(request);
-
-module.exports=requestAsync;
