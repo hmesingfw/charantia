09f4aa2c61aa997854ab0cad5971e6870da464a5
xiaomian <812083426@qq.com>
2019-10-24 11:09:32 +0800

    0.60

--- vue-admin/babel.config.js
+++ vue-admin/babel.config.js
@@ -1,5 +1,13 @@
+
+const plugins = ["@vue/babel-plugin-transform-vue-jsx"]
+// 生产环境移除console
+if (process.env.NODE_ENV === 'production') {
+    plugins.push("transform-remove-console")
+}
+
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+    plugins: plugins,
+    presets: [
+        '@vue/app'
+    ]
 }
