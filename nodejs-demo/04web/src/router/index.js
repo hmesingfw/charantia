6815e3b10fb6a9e5e79c977ce6b0fa568b58945a
xiaomian <812083426@qq.com>
2019-11-10 21:39:03 +0800

    update 04web

--- nodejs-demo/04web/src/router/index.js
+++ nodejs-demo/04web/src/router/index.js
@@ -37,17 +37,14 @@ import Layout from '@/layout'
  * a base page that does not have permission requirements
  * all roles can be accessed
  */
-export const constantRoutes = [
-    {
+export const constantRoutes = [{
         path: '/redirect',
         component: Layout,
         hidden: true,
-        children: [
-            {
-                path: '/redirect/:path*',
-                component: () => import('@/views/redirect/index')
-            }
-        ]
+        children: [{
+            path: '/redirect/:path*',
+            component: () => import('@/views/redirect/index')
+        }]
     },
     {
         path: '/login',
@@ -73,27 +70,31 @@ export const constantRoutes = [
         path: '/admin',
         component: Layout,
         redirect: '/dashboard',
-        children: [
-            {
-                path: 'dashboard',
-                component: () => import('@/views/dashboard/index'),
-                name: 'Dashboard',
-                meta: { title: '首页', icon: 'dashboard', affix: true }
+        children: [{
+            path: 'dashboard',
+            component: () => import('@/views/dashboard/index'),
+            name: 'Dashboard',
+            meta: {
+                title: '首页',
+                icon: 'dashboard',
+                affix: true
             }
-        ]
+        }]
     },
     {
         path: '/icon',
         component: Layout,
         hidden: true,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/icons/index'),
-                name: 'Icons',
-                meta: { title: '图标', icon: 'icon', noCache: true }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/icons/index'),
+            name: 'Icons',
+            meta: {
+                title: '图标',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }]
     },
     {
         path: '/error',
@@ -105,18 +106,23 @@ export const constantRoutes = [
             title: '错误页面',
             icon: '404'
         },
-        children: [
-            {
+        children: [{
                 path: '401',
                 component: () => import('@/views/error-page/401'),
                 name: 'Page401',
-                meta: { title: '401', noCache: true }
+                meta: {
+                    title: '401',
+                    noCache: true
+                }
             },
             {
                 path: '404',
                 component: () => import('@/views/error-page/404'),
                 name: 'Page404',
-                meta: { title: '404', noCache: true }
+                meta: {
+                    title: '404',
+                    noCache: true
+                }
             }
         ]
     },
@@ -126,14 +132,15 @@ export const constantRoutes = [
         path: '/canvas',
         component: Layout,
         hidden: true,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/temp/canvas'),
-                name: 'canvas',
-                meta: { title: '样例', icon: 'user' }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/temp/canvas'),
+            name: 'canvas',
+            meta: {
+                title: '样例',
+                icon: 'user'
             }
-        ]
+        }]
     },
 
 
@@ -141,102 +148,128 @@ export const constantRoutes = [
     {
         path: '/patients',
         component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/patients/index'),
-                name: 'patients',
-                meta: { title: '病人信息', icon: 'icon', noCache: true }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/patients/index'),
+            name: 'patients',
+            meta: {
+                title: '病人信息',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }]
     },
     {
         path: '/doctor',
         component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/doctor/index'),
-                name: 'doctor',
-                meta: { title: '医生信息', icon: 'icon', noCache: true }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/doctor/index'),
+            name: 'doctor',
+            meta: {
+                title: '医生信息',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }]
     },
     {
         path: '/sickbed',
         component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/sickbed/index'),
-                name: 'sickbed',
-                meta: { title: '病床信息', icon: 'icon', noCache: true }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/sickbed/index'),
+            name: 'sickbed',
+            meta: {
+                title: '病床信息',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }]
     },
     {
         path: '/item',
         component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/item/index'),
-                name: 'item',
-                meta: { title: '项目信息', icon: 'icon', noCache: true }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/item/index'),
+            name: 'item',
+            meta: {
+                title: '项目信息',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }]
     },
     {
         path: '/notes',
         component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/notes/index'),
-                name: 'notes',
-                meta: { title: '重要事项', icon: 'icon', noCache: true }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/notes/index'),
+            name: 'notes',
+            meta: {
+                title: '重要事项',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }]
     },
-    
+
     {
         path: '/basedata',
         component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/basedata/index'),
-                name: 'basedata',
-                meta: { title: '基础数据', icon: 'icon', noCache: true }
+        children: [{
+            path: 'index',
+            component: () => import('@/views/basedata/index'),
+            name: 'basedata',
+            meta: {
+                title: '基础数据',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }]
     },
- 
+
 
 
 
     {
         path: '/sys',
         component: Layout,
-        meta: { title: '平台管理', icon: 'dashboard', affix: true },
-        children: [
-            {
-                path: 'sysuser',
-                component: () => import('@/views/sysuser/index'),
-                name: 'sysuser',
-                meta: { title: '人员管理', icon: 'icon', noCache: true }
-            }, {
-                path: 'sysrole',
-                component: () => import('@/views/sysrole/index'),
-                name: 'sysrole',
-                meta: { title: '角色管理', icon: 'icon', noCache: true }
-            }, {
-                path: 'syspermission',
-                component: () => import('@/views/router-view/index'),
-                name: 'syspermission',
-                meta: { title: '菜单管理', icon: 'icon', noCache: true }
+        meta: {
+            title: '平台管理',
+            icon: 'dashboard',
+            affix: true
+        },
+        children: [{
+            path: 'sysuser',
+            component: () => import('@/views/sysuser/index'),
+            name: 'sysuser',
+            meta: {
+                title: '人员管理',
+                icon: 'icon',
+                noCache: true
             }
-        ]
+        }, {
+            path: 'sysrole',
+            component: () => import('@/views/sysrole/index'),
+            name: 'sysrole',
+            meta: {
+                title: '角色管理',
+                icon: 'icon',
+                noCache: true
+            }
+        }, {
+            path: 'syspermission',
+            component: () => import('@/views/router-view/index'),
+            name: 'syspermission',
+            meta: {
+                title: '菜单管理',
+                icon: 'icon',
+                noCache: true
+            }
+        }]
     },
 
     {
@@ -245,17 +278,28 @@ export const constantRoutes = [
         hidden: true
     },
     {
-        path: '/',
+        path: '/oldindex',
         component: () => import('@/views/panel/index'),
         hidden: true
     },
+    {
+        path: '/',
+        component: () => import('@/views/panel/newIndex'),
+        hidden: true
+    },
 
-    { path: '*', redirect: '/404', hidden: true }
+    {
+        path: '*',
+        redirect: '/404',
+        hidden: true
+    }
 ]
 
 const createRouter = () => new Router({
     // mode: 'history', // require service support
-    scrollBehavior: () => ({ y: 0 }),
+    scrollBehavior: () => ({
+        y: 0
+    }),
     routes: constantRoutes
 })
 
