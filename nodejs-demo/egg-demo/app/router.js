6550d68c91d6ca98a0da052935e24f0d84592576
xiaomian <812083426@qq.comm>
2020-04-25 22:38:17 +0800

    更新egg video

--- nodejs-demo/egg-demo/app/router.js
+++ nodejs-demo/egg-demo/app/router.js
@@ -4,6 +4,6 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
+    const { router, controller } = app;
+    router.get('/video', controller.home.index);
 };
